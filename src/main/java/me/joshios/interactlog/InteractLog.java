a5e926bc89047fe82de63b0a216430cef656c8d2
Joshua <joshuafranco1313@gmail.com>
2022-05-30 18:47:30 -0400

    Add InteractLog#registerEvents and send log on startup/shutdown

--- src/main/java/me/joshios/interactlog/InteractLog.java
+++ src/main/java/me/joshios/interactlog/InteractLog.java
@@ -5,22 +5,34 @@ import me.joshios.interactlog.event.types.InventoryEventListener;
 import me.joshios.interactlog.event.types.ItemDropListener;
 import me.joshios.interactlog.log.InteractionLogCommand;
 import me.joshios.interactlog.user.InteractionUserRepository;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InteractLog extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("Enabling InteractLog " + getDescription().getVersion() + " created by Joshios");
+
         InteractionUserRepository repository = new InteractionUserRepository();
         InteractionEventService service = new InteractionEventService(repository);
 
-        getServer().getPluginManager().registerEvents(new ItemDropListener(service), this);
-        getServer().getPluginManager().registerEvents(new InventoryEventListener(service), this);
+        registerEvents(
+            new ItemDropListener(service),
+            new InventoryEventListener(service)
+        );
+
         getCommand("logs").setExecutor(new InteractionLogCommand(repository));
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("Disabling InteractLog " + getDescription().getVersion() + " created by Joshios");
+    }
 
+    private void registerEvents(Listener... listeners) {
+        for (Listener listener : listeners) {
+            getServer().getPluginManager().registerEvents(listener, this);
+        }
     }
 }
